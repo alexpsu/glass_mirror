b6f95efee1b0f4d69e040687ad295667f740d35b
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-19 15:09:43 -0800

    Generated splash controller and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'splash/index'
+
+  get 'splash/show'
+
   #Routes for CRUD for Users
   get "/users", to: "users#index", as: "users"
   get "/signup", to: "users#new", as: "new_user"
