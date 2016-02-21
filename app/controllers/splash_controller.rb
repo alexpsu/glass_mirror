4ad826606ae89d4ea4c0b2767abe2e3f8999110e
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-21 12:49:25 -0800

    Fixed up user show page. Added button to add job

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,5 +1,8 @@
 class SplashController < ApplicationController
   def index
+    if current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,8 @@ Rails.application.routes.draw do
 
   #Routes CRUD jobs
   get "/jobs/:id", to: "jobs#show", as: "job"
-  post "/users/:id/jobs", to: "jobs#new", as: "new_job"
+  get "/users/:id/jobs", to: "jobs#new", as: "new_job"
+  post '/jobs', to:'jobs#create'
   get "/jobs/:id/edit", to: "jobs#edit", as: "edit_job"
   patch "/jobs/:id", to: "jobs#update"
   delete "/jobs/:id", to: "jobs#destroy"
