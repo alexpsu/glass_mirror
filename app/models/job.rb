f453bdf179926d1ab55fe4f42c9fcd1481021c24
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-22 14:20:05 -0800

    Added method in job model to sort

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,9 @@ class Job < ActiveRecord::Base
   belongs_to :user
   has_many :milestones, :dependent => :delete_all
   accepts_nested_attributes_for :milestones, :reject_if => lambda { |b| b[:title].blank? }
+
+
+  def sort_mil
+    self.milestones.order(:id)
+  end
 end
